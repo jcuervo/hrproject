3a65a4534f5224fe45386c0b0d5f0059785c871c
Moanna May Renomeron <mmrenomeron@gmail.com>
2011-10-05 19:40:24 +0800

    Commit applicant front_view (multi-step, save)

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -9,9 +9,35 @@ class Applicant < ActiveRecord::Base
   has_many :interview_ratings
   accepts_nested_attributes_for :educations, :allow_destroy => true
 
-  validates :first_name, :middle_name, :last_name, :gender, :civil_status, :street, :city, :region, :zip,
-            :birthdate, :landline, :mobile, :email, :date_available_for_work, :desired_salary_range, :presence => true
-  validates :email, :uniqueness => true
-  validates :email, :format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i }
+  #validates :first_name, :middle_name, :last_name, :presence => true
+  #validates :email, :uniqueness => true
+  #validates :email, :format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i }
 
+  attr_accessible :first_name, :middle_name, :last_name
+  attr_writer :current_step
+
+  def current_step
+    @current_step || steps.first
+  end
+
+  def steps
+    %w[personal_info education]
+  end
+
+  def next_step
+    self.current_step = steps[steps.index(current_step)+1]
+  end
+
+  def previous_step
+    self.current_step = steps[steps.index(current_step)-1]
+  end
+
+  def first_step?
+    current_step == steps.first
+  end
+
+  def last_step?
+    current_step == steps.last
+  end
 end
+
