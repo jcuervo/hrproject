f2ed8922ddeecb357f0650db2e428efe9c2b6169
Moanna May Renomeron <mmrenomeron@gmail.com>
2011-10-07 14:46:16 +0800

    reference form added

---
+++ config/initializers/formtastic.rb
@@ -0,0 +1,17 @@
+module Formtastic
+  module DatePicker
+    protected
+ 
+    def datepicker_input(method, options = {})
+      format = format = options[:format] || Date::DATE_FORMATS[:default] || "%d %b %Y"
+      string_input(method, datepicker_options(format, object.send(method)).merge(options))
+    end
+ 
+    # Generate html input options for the datepicker_input
+    #
+    def datepicker_options(format, value = nil)
+      datepicker_options = {:value => value.try(:strftime, format), :input_html => {:class => 'ui-datepicker'}}
+    end
+  end
+end
+Formtastic::SemanticFormBuilder.send(:include, Formtastic::DatePicker)
